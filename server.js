ebde8f54c110943d9c1a5c0dc06c742f69e25cdf
Robert Godlewski <robtechg5@gmail.com>
2022-04-03 20:31:16 -0700

    using faker and creates a new user every api call.

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 //import express from "express";
 const express = require("express");
+//import faker from "faker";
+const faker = require("faker");
 
 const app = express();
 const port = 800;
 
-app.get("/api/hello", (req, res) => {
+//app.get("/api/hello", (req, res) => {
     //res.send("Hello World");
-    res.json({message: "Hello World"});
+    //res.json({message: "Hello World"});
+//});
+
+const generateUser = () => {
+    const newFake = {
+        _id: faker.datatype.uuid(),
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        phoneNumber: faker.phone.phoneNumber(),
+        email: faker.internet.email(),
+        password: faker.internet.password()
+    };
+    return newFake;
+};
+
+app.get("/api/users/new", (req, res) => {
+    const newUser = generateUser();
+    res.json(newUser);
 });
 
 app.listen(port, () => {
