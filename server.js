a8d13e399ac70c229894d4ae0779d1d8239e291d
Robert Godlewski <robtechg5@gmail.com>
2022-04-03 20:25:49 -0700

    Created Express app.

---
+++ server.js
@@ -0,0 +1,14 @@
+//import express from "express";
+const express = require("express");
+
+const app = express();
+const port = 800;
+
+app.get("/api/hello", (req, res) => {
+    //res.send("Hello World");
+    res.json({message: "Hello World"});
+});
+
+app.listen(port, () => {
+    console.log(`Express server is listening on port ${port}`);
+});
