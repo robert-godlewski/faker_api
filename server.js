5c397b904332260119290ff97887b0767eecc47e
Robert Godlewski <robtechg5@gmail.com>
2022-04-03 20:42:31 -0700

    creates a new company and user-company relation api calls.

--- server.js
+++ server.js
@@ -23,9 +23,39 @@ const generateUser = () => {
     return newFake;
 };
 
+const generateCompany = () => {
+    const newFake = {
+        _id: faker.datatype.uuid(),
+        name: faker.company.companyName(),
+        address: {
+            street: faker.address.streetAddress(),
+            city: faker.address.cityName(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country: faker.address.country()
+        }
+    };
+    return newFake;
+};
+
 app.get("/api/users/new", (req, res) => {
     const newUser = generateUser();
     res.json(newUser);
+});
+
+app.get("/api/companies/new", (req, res) => {
+    const newCompany = generateCompany();
+    res.json(newCompany);
+});
+
+app.get("/api/user/company", (req, res) => {
+    const newUser = generateUser();
+    const newCompany = generateCompany();
+    const user_company = {
+        user: newUser,
+        company: newCompany
+    };
+    res.json(user_company);
 });
 
 app.listen(port, () => {
